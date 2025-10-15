be0119a1d80195f4acc1872ea6e40773836b57b3
Julien Esterbet <julien.esterbet@gmail.com>
2025-10-15 11:19:55 +0200

    Update from 2022

--- src/components/main/header/Header.jsx
+++ src/components/main/header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         <CTA />
 
         <div className="me">
-          <img src={ME} alt="me" />
+          <img src={ME} alt="me"/>
         </div>
         <HeaderSocials />
 
