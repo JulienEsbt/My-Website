7e2983eb2956434874c6b46f5832175ba017b165
Julien Esterbet <julien.esterbet@gmail.com>
2026-08-13 13:35:13 +0200

    feat: add privacy-conscious Vercel insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
+import {Analytics} from '@vercel/analytics/react'
+import {SpeedInsights} from '@vercel/speed-insights/react'
 import {MotionConfig} from 'framer-motion'
 import {BrowserRouter} from 'react-router-dom'
 import Router from './app/router.jsx'
 import AppShell from './app/AppShell.jsx'
 
 const App = () => (
-    <MotionConfig reducedMotion="user">
-        <BrowserRouter>
-            <AppShell>
-                <Router />
-            </AppShell>
-        </BrowserRouter>
-    </MotionConfig>
+    <>
+        <MotionConfig reducedMotion="user">
+            <BrowserRouter>
+                <AppShell>
+                    <Router />
+                </AppShell>
+            </BrowserRouter>
+        </MotionConfig>
+        <Analytics />
+        <SpeedInsights />
+    </>
 )
 
 export default App
